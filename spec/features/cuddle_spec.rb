b8f5f4a1e0e95eb6a4c0a903c480693dd878895d
Caio Andrade <caioertai@gmail.com>
2018-11-04 19:06:31 -0600

    Chaged specs

--- spec/features/cuddle_spec.rb
+++ spec/features/cuddle_spec.rb
@@ -1,15 +1,12 @@
 require "rails_helper"
 
 RSpec.feature "Cuddling", :type => :feature do
-  scenario "User searches for a pet to cuddle" do
+  scenario "User sees a list of pets to cuddle" do
     # Setup
     Pet.create(name: 'Mr. Cuddlesworth', species: 'Cat')
 
     # Exercise
     visit "/"
-
-    fill_in "Search", with: "Cat"
-    click_button "Cuddle Search!"
 
     # Verify
     expect(page).to have_text("Mr. Cuddlesworth")
