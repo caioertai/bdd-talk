1ae644cb4a08dca78c1797d9dbb623735ed4f96e
Caio Andrade <caioertai@gmail.com>
2018-11-13 18:18:48 -0600

    Moved spec

--- features/step_definitions/localhost.rb
+++ features/step_definitions/localhost.rb
@@ -1,10 +1,10 @@
-When("the app has a pet named {string}") do |pet_name|
-  Pet.create(name: pet_name, species: 'Cat')
-end
-
 When("the User visits the home") do
   visit "/"
 end
+
+When("the app has a pet named {string}") do |pet_name|
+  Pet.create(name: pet_name, species: 'Cat')
+end  
 
 Then("the User sees {string}") do |text|
   expect(page).to have_text(text)
